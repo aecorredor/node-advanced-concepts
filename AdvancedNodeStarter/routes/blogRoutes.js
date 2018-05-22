7eb7032935aa279b06482bd2fdc38345f51123be
Alejandro Corredor <alejandro@alejandros-mbp.lan>
2018-05-22 00:13:45 -0400

    fix submodule issue

---
+++ AdvancedNodeStarter/routes/blogRoutes.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const requireLogin = require('../middlewares/requireLogin');
+
+const Blog = mongoose.model('Blog');
+
+module.exports = app => {
+  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+    const blog = await Blog.findOne({
+      _user: req.user.id,
+      _id: req.params.id
+    });
+
+    res.send(blog);
+  });
+
+  app.get('/api/blogs', requireLogin, async (req, res) => {
+    const redis = require('redis');
+    const redisURL = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisURL);
+    const util = require('util');
+    client.get = util.promisify(client.get);
+
+    // Do we have any cached data in redis related to this query?
+    const cachedBlogs = await client.get(req.user.id);
+
+    // If yes, then respond to the request righ away and return
+    if (cachedBlogs) {
+      console.log('serving from cache');
+      return res.send(JSON.parse(cachedBlogs));
+    }
+
+    // If no, we need to respond to request and update our cache to store the data
+    const blogs = await Blog.find({ _user: req.user.id });
+
+    console.log('serving from mongo');
+    res.send(blogs);
+    client.set(req.user.id, JSON.stringify(blogs));
+  });
+
+  app.post('/api/blogs', requireLogin, async (req, res) => {
+    const { title, content } = req.body;
+
+    const blog = new Blog({
+      title,
+      content,
+      _user: req.user.id
+    });
+
+    try {
+      await blog.save();
+      res.send(blog);
+    } catch (err) {
+      res.send(400, err);
+    }
+  });
+};
