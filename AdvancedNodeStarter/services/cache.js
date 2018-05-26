925ed58f6519ddf019fc974dd6e219539d451633
Alejandro Corredor <alejandro@alejandros-mbp.lan>
2018-05-26 14:10:59 -0400

    finish implementing redis caching layer

--- AdvancedNodeStarter/services/cache.js
+++ AdvancedNodeStarter/services/cache.js
@@ -1,16 +1,55 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
 
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () {
-  console.log('I am about to run a query');
+mongoose.Query.prototype.cache = function(options = {}) {
+  this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || '');
 
-  const key = {
+  // returning 'this' makes this function chainable like other Query instance functions
+  return this;
+};
+
+mongoose.Query.prototype.exec = async function () {
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
+
+  const key = JSON.stringify({
     ...this.getQuery(),
     collection: this.mongooseCollection.name,
-  };
+  });
 
-  console.log(key);
+  // See if we have a value for 'key' in redis
+  const cacheValue = await client.hget(this.hashKey, key);
 
-  return exec.apply(this, arguments);
+  // If we do, return that
+  if (cacheValue) {
+    // exec is supposed to return a mongoose model
+    const doc = JSON.parse(cacheValue);
+
+    // If we get an array of results we have to turn each one into a mongoose model
+    if (Array.isArray(doc)) {
+      return doc.map(item => new this.model(item));
+    }
+
+    return new this.model(doc);
+  }
+
+  // Otherwise, issue the query and store the result in redis
+  const result = await exec.apply(this, arguments);
+  client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
+
+  return result;
+}
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  }
 }
